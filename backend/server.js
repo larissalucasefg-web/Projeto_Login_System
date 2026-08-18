66ed6546e6db41dce69da5d1290b494484978a9a
Larissa Lucas <larissalucas.efg@gmail.com>
2026-08-17 21:23:49 -0300

    Criando o Backend e Rotas

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+const PORT = 3001
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.use("/api", authRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);;
+});
